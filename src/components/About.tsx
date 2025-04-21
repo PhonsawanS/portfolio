c0ec81770ff0c1856dd3a4f366f488c56a3b2e2f
Sky <146667716+PhonsawanS@users.noreply.github.com>
2025-04-21 20:10:18 +0700

    Edit Home About add README

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -19,9 +19,11 @@ const About = () => {
           {/* Left Column - Image */}
           <div className="md:w-1/3">
             <div className="rounded-md overflow-hidden shadow-md">
-              <img
+              <Image
                 src="/About.jpg"
                 alt="Phonsawan Suarab"
+                width={300} // หรือขนาดที่คุณต้องการ
+                height={400}
                 className="w-md h-auto object-cover"
               />
             </div>
@@ -99,7 +101,7 @@ const About = () => {
           <div className="md:w-2/3">
             {/* <h3 className="text-2xl font-semibold mb-4">Web Developer & UI/UX Designer</h3> */}
             <p className="text-gray-700 mb-6">
-              Hello! I'm Phonsawan. I recently graduated and have a growing interest in back-end development.
+              Hello! I&rsquo;m Phonsawan. I recently graduated and have a growing interest in back-end development.
               I'm open to learning, practicing, and working with others to improve my skills step by step.
               I believe in learning by doing and look forward to opportunities where I can contribute and grow.
             </p>
@@ -126,7 +128,7 @@ const About = () => {
               <div className="space-y-6">
                 <div className="border-l-4 border-green-500 pl-4">
                   <div className="text-sm text-gray-500">2021-2025</div>
-                  <h4 className="text-md font-medium">Bachelor's Degree in Computer Engineering</h4>
+                  <h4 className="text-md font-medium">Bachelor&rsquo;s Degree in Computer Engineering</h4>
                   <p className="text-gray-700">Faculty of Engineering, Naresuan University</p>
                 </div>
               </div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Download } from "lucide-react";
+import Image from 'next/image';
 const Home = () => {
   return (
     <div id="home" className="min-h-screen flex items-center justify-center  text-gray-800">
@@ -8,7 +9,7 @@ const Home = () => {
           <div className="md:w-2/3 text-center md:text-left">
             <p className="text-sm text-gray-600 mb-2">Hello! I Am</p>
             <h1 className="text-4xl md:text-5xl font-bold mb-2 text-gray-900">Phonsawan Suarab</h1>
-            <p className="text-lg text-gray-700">I'm a Web Developer</p>
+            <p className="text-lg text-gray-700">I&rsquo;m a Web Developer</p>
 
             <div className="mt-6 flex gap-4 justify-center md:justify-start">
               <a href="#contact" className="px-4 py-2 border border-blue-500 text-blue-500 rounded hover:bg-blue-500 hover:text-white transition-all">Contact</a>
@@ -26,9 +27,11 @@ const Home = () => {
 
           <div className="md:w-1/3 mt-8 md:mt-0 flex justify-center">
             <div className="w-64 h-64 rounded overflow-hidden border-2 border-gray-300 shadow-lg">
-              <img
+              <Image
                 src="/profile.png"
                 alt="Phonsawan Suarab"
+                width={300} // หรือขนาดที่คุณต้องการ
+                height={400}
                 className="w-full h-full object-cover"
               />
             </div>
